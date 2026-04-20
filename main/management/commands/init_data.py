9011b6ca8123fabfa1a1658e6103f5327d627eab
kartinkak76-commits <kartinkak76@gmail.com>
2026-04-20 22:54:19 +0300

    image burger

--- main/management/commands/init_data.py
+++ main/management/commands/init_data.py
@@ -26,7 +26,7 @@ class Command(BaseCommand):
         vegetarian = BurgerCategory.objects.get(name='Вегетарианские')
         chicken = BurgerCategory.objects.get(name='Куриные')
         
-        # Создаем бургеры
+        # Создаем бургеры с URL изображений
         burgers = [
             {
                 'name': 'Чизбургер Классический',
@@ -34,7 +34,8 @@ class Command(BaseCommand):
                 'description': 'Сочная говяжья котлета, расплавленный сыр чеддер, свежие овощи и фирменный соус',
                 'composition': 'Говяжья котлета, сыр чеддер, салат, помидоры, лук, соус',
                 'price': 250,
-                'calories': 520
+                'calories': 520,
+                'image_url': 'https://i.postimg.cc/8PVrrbmX/burger.png'
             },
             {
                 'name': 'Двойной Гурман',
@@ -42,7 +43,8 @@ class Command(BaseCommand):
                 'description': 'Две сочные котлеты, двойная порция сыра, бекон и специальный соус',
                 'composition': '2 говяжьи котлеты, сыр чеддер, бекон, салат, помидоры, соус',
                 'price': 380,
-                'calories': 780
+                'calories': 780,
+                'image_url': 'https://avatars.mds.yandex.net/get-shedevrum/14550299/img_65229b487ff811efb254c6c9289df30b/orig'
             },
             {
                 'name': 'Острый Техасский',
@@ -50,7 +52,8 @@ class Command(BaseCommand):
                 'description': 'Пикантная котлета с халапеньо, острый соус и сыр с перцем',
                 'composition': 'Говяжья котлета с перцем, сыр с халапеньо, салат, острый соус',
                 'price': 320,
-                'calories': 590
+                'calories': 590,
+                'image_url': 'https://static.tildacdn.com/tild3437-6138-4261-b463-376166363534/_____24.png'
             },
             {
                 'name': 'Вегетарианский Гарден',
@@ -58,7 +61,8 @@ class Command(BaseCommand):
                 'description': 'Нутовая котлета, свежие овощи, авокадо и веганский соус',
                 'composition': 'Нутовая котлета, авокадо, салат, помидоры, огурцы, веганский соус',
                 'price': 290,
-                'calories': 430
+                'calories': 430,
+                'image_url': 'https://img.povar.ru/mobile/c9/41/7b/34/vegetarianskii_burger_master-klass-510146.JPG'
             },
             {
                 'name': 'Куриный Кранч',
@@ -66,7 +70,8 @@ class Command(BaseCommand):
                 'description': 'Хрустящая куриная котлета в панировке, сыр и соус ранч',
                 'composition': 'Куриная котлета, сыр, салат, помидоры, соус ранч',
                 'price': 270,
-                'calories': 480
+                'calories': 480,
+                'image_url': 'https://avatars.mds.yandex.net/i?id=e9a7f97565fe9dc1e2230a3b0ca9c475_l-5176811-images-thumbs&n=13'
             },
             {
                 'name': 'BBQ Бургер',
@@ -74,7 +79,8 @@ class Command(BaseCommand):
                 'description': 'Котлета из мраморной говядины, карамелизованный лук, бекон и BBQ соус',
                 'composition': 'Мраморная говядина, бекон, сыр, карамелизованный лук, BBQ соус',
                 'price': 350,
-                'calories': 650
+                'calories': 650,
+                'image_url': 'https://avatars.mds.yandex.net/get-eda/3808326/8ff58a9276f22ae11de4258561bed4fd/1200x1200nocrop'
             },
         ]
         
@@ -87,37 +93,43 @@ class Command(BaseCommand):
                     'composition': burger_data['composition'],
                     'price': burger_data['price'],
                     'calories': burger_data['calories'],
+                    'image_url': burger_data['image_url'],
                     'is_available': True
                 }
             )
             if created:
                 self.stdout.write(self.style.SUCCESS(f'Создан бургер: {burger.name}'))
+            else:
+                # Если бургер уже существует, обновляем URL изображения
+                burger.image_url = burger_data['image_url']
+                burger.save()
+                self.stdout.write(self.style.WARNING(f'Обновлён бургер: {burger.name}'))
         
         # Создаем точки выдачи
         points = [
             {
-                'name': 'ТРЦ "Галерея"',
-                'address': 'ул. Ленина, 10, 1 этаж',
-                'latitude': 55.7558,
-                'longitude': 37.6176,
-                'working_hours': '10:00 - 22:00',
-                'phone': '+7 (999) 111-11-11'
+                'name': 'ТЦ "Звёздный"',
+                'address': 'Коммунистическая ул., 7, Сыктывкар',
+                'latitude': 61.6643,
+                'longitude': 50.835,
+                'working_hours': '10:00 - 21:00',
+                'phone': '+7 (904) 271-16-05'
             },
             {
-                'name': 'Метро "Парк культуры"',
-                'address': 'Зубовский бульвар, 15',
-                'latitude': 55.7345,
-                'longitude': 37.5900,
-                'working_hours': '09:00 - 21:00',
-                'phone': '+7 (999) 222-22-22'
+                'name': 'ТЦ "Июнь"',
+                'address': 'Октябрьский пр., 133, Сыктывкар',
+                'latitude': 61.6489,
+                'longitude': 50.8094,
+                'working_hours': '09:00 - 22:00',
+                'phone': '+7 (8212) 28-88-88'
             },
             {
-                'name': 'БЦ "Москва-Сити"',
-                'address': 'Пресненская наб., 8, -1 этаж',
-                'latitude': 55.7475,
-                'longitude': 37.5400,
-                'working_hours': '11:00 - 23:00',
-                'phone': '+7 (999) 333-33-33'
+                'name': 'ТЦ "Парма"',
+                'address': 'Коммунистическая ул., 50, Сыктывкар',
+                'latitude': 61.6684,
+                'longitude': 50.8359,
+                'working_hours': '10:00 - 21:00',
+                'phone': '+7 (8212) 46-86-03'
             },
         ]
         
